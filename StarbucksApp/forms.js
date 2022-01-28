97638dc0304d745dd0fdc3b0cbf555005c0e994e
Jeffrey Lamar <jlam3950@gmail.com>
2022-01-28 15:32:32 -0500

    adding projects

---
+++ StarbucksApp/forms.js
@@ -0,0 +1,8 @@
+
+const buttonclick = document.getElementById("submit-button");
+
+buttonclick.addEventListener("click",sayHi());
+
+function sayHi(){
+    alert("Thanks for applying! This took me way too long to figure out. - JL");
+}
