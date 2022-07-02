edc3a94fd75ca4e6c7f04e79ce285814777fa7bc
Jeffrey Lamar <jlam3950@gmail.com>
2022-07-02 17:52:34 -0400

    updated app to include weather searches by zip. updated CSS styling, and event listeners

---
+++ SpotifyAPP/public/js/app.js
@@ -0,0 +1,112 @@
+const searchBar = document.getElementById('searchBar');
+const searchBtn = document.getElementById('searchBtn');
+const sunny = document.getElementById('sunny');
+const hipHop = document.getElementById('hipHop');
+const subBtn = document.getElementById('submit');
+const weatherContainer = document.getElementById('forecast');
+
+let param1;
+let param2;
+let genre; 
+
+
+async function getWeather(){
+    const address = searchBar.value; 
+    const url = `https://api.geocod.io/v1.6/geocode?q=${address}&api_key=661f10101f5551601645516fff5559650249f94`;
+
+    try{
+        const response = await fetch(url, {
+            method: 'GET'
+        })
+        const data = await response.json();
+        weatherAPI(data);
+    } catch (error){
+        console.log(error);
+    }
+}
+
+async function weatherAPI(data){
+    const lat = data.results[0].location.lat;
+    const lng = data.results[0].location.lng;
+
+    const url = `https://api.weather.gov/points/${lat},${lng}`
+    try{
+        const response = await fetch(url, {
+            method: 'GET'
+        });
+        const data = await response.json();
+        forecastAPI(data);
+
+    } catch(error){
+        console.log(error);
+    }
+}
+
+function forecastAPI(data){
+
+    const url = data.properties.forecast; 
+
+    fetch(url).then(res => res.json()).then(forecastResponse =>{
+        console.log(forecastResponse.properties.periods)
+        let data = forecastResponse.properties.periods;
+        forecastRender(data);
+    })
+}
+
+function forecastRender(forecastData){
+    forecastData.forEach((day , index) => {
+        if(index % 2 == 0){
+        weatherContainer.innerHTML += 
+        `<div class = weatherCard>
+        <p> ${day.name} </p>
+        <p> ${day.shortForecast} </p>
+        <p> ${day.temperature} </p>
+        <img src="${day.icon}">`
+
+        }
+    })
+}
+
+searchBtn.addEventListener('click', function(e){
+    e.preventDefault()
+    weatherContainer.innerHTML = '';
+    getWeather();
+});
+
+sunny.addEventListener('click', function(){
+    console.log('hi');
+    param1 = '0c6xIDDpzE81m2q797ordA';
+    param2 ='90';
+    genre= 'classical';
+    sunny.style.background = 'red';
+});
+
+// hipHop.addEventListener('click', function(){
+    //     genre += 'hip-hop';
+    // })
+    
+    
+async function callRec(tr, pop, genre) {
+    // need access_token for request
+    const url = `https://api.spotify.com/v1/recommendations?seed_tracks=${tr}&limit=10&max_popularity=${pop}&seed_genres=${genre}`;
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + 'BQCHq7i9HCe_QYCdGQbbn_ipUT6rC5BLarrA6iYvqYrbrWIo01LZSabB5c7w1TFCKQhoIb0rINVeDgShNiXDurj52Z14lY2XrQCcWhz2XbfJ_nBaAhpm-g_VLYWM94nFLrYOrX6yRkdLRJgdw4THQfKI310ewP3ZVNQbWYqPK44Uea4t0yVDWgDaKhxEY90xNQ'
+            }
+        });
+
+        const data = await response.json();
+        console.log(data);
+        console.log(data.tracks[0]);
+    } catch (error) {
+        console.log(error);
+    }
+}
+    
+subBtn.addEventListener('click', function(e){
+    e.preventDefault()
+    callRec(param1,param2,genre)
+});
+// https://api.spotify.com/v1/recommendations?seed_tracks=0c6xIDDpzE81m2q797ordA&limit=10&max_popularity=90&seed_genres='classical'
--- SpotifyAPP/server.js
+++ SpotifyAPP/server.js
@@ -1,20 +1,32 @@
+require('dotenv').config();
 const { response } = require('express');
 const express = require('express');
-// const spotifyRoutes = require('./src/spotify/routes')
+const spotifyRoutes = require('./src/spotify/routes')
 const app = express();
 const PORT = 3000; 
 const fetch = require('node-fetch');
+const pool = require('./db');
+
 
 app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static('public'));
 app.use(express.json());
-// app.use('/api/spotify', spotifyRoutes);
+app.use('/api/spotify', spotifyRoutes);
 app.set('view engine', 'ejs');
 
 const redirect_uri = 'http://localhost:3000/callback';
-const client_id = 'd953cd347dda4a9d868ce4ff54ae8983';
-const client_secret = '69b2b6da93404c5997ef18c4ef9ebbe4';
+const client = process.env.CLIENT_ID; 
+const secret = process.env.CLIENT_SECRET;
+let access_token;
 
 app.get('/', (req,res) => {
+    pool.query('SELECT * FROM users', (err,res)=>{
+        if(!err){
+            console.log(res.rows);
+        }else{
+            console.log(err);
+        }
+    });
     res.render('index');
 })
 
@@ -25,36 +37,17 @@ app.get('/home', (req,res) => {
 app.get('/authorize', (req,res) => {
     const auth_query_parameters = new URLSearchParams({
         response_type: 'code',
-        client_id: 'd953cd347dda4a9d868ce4ff54ae8983',
+        client_id: client, 
         scope:'user-read-private user-read-email',
         redirect_uri: 'http://localhost:3000/callback',
         state: '34fFs29kd09'
     })
-
-    // console.log(auth_query_parameters.toString());
-    // console.log(("http://accounts.spotify.com/authorize?" + auth_query_parameters.toString()))
-
     res.redirect("http://accounts.spotify.com/authorize?" + auth_query_parameters.toString());
 });
 
 app.get('/callback', async (req,res) => {
     
     const code = req.query.code; 
-    console.log(code);
-
-    // SPOTIFY_TOKEN = 'https://accounts.spotify.com/api/token'
-    // request_body = {
-    //     'grant_type': 'authorization_code',
-    //     'code': code,
-    //     'redirect_uri': redirect_uri,
-    //     'client_id': 'd953cd347dda4a9d868ce4ff54ae8983',
-    //     'client_secret': '69b2b6da93404c5997ef18c4ef9ebbe4'
-    // }
-
-    // r = requests.post(url=SPOTIFY_TOKEN, data=request_body)
-    // resp = r.json();
-    // console.log(resp);
-    
 
     const body = new URLSearchParams({
         code: code,
@@ -68,97 +61,38 @@ app.get('/callback', async (req,res) => {
         headers: {
             'Content-type': 'application/x-www-form-urlencoded',
             'Authorization':
-            'Basic ' + Buffer.from(`${client_id}:${client_secret}`).toString('base64')
+            'Basic ' + Buffer.from(`${client}:${secret}`).toString('base64')
         },
     })
 
     const data = await response.json(); 
-    console.log(data);
-    res.render('callback');
+    access_token = data.access_token;
+    // localStorage.setItem('token', access_token);
+   
+    res.redirect('search');
 });
 
+async function endPointData(endpoint){
+    const response = await fetch('https://api.spotify.com/v1' + endpoint, {
+        method: 'GET',
+        headers: {
+            Authorization: 'Bearer ' + access_token
+        }
+    });
+    console.log('Bearer ' + access_token);
+    //consider pushing information to db, create function (take response)
+    const data = await response.json();
+    return data;
+}
 
+app.get('/search', async (req,res) => {
 
-
-// app.get('/about', (req,res) => {
-//     res.render('about');
-// })
-
-// app.get('/users/code', (req,res) => {
-//     res.render('about');
-// })
-
-
-// app.get('/token', async function (req, res, next) {
-    
-//     let base_url = 'https://accounts.spotify.com'
-//     let authentication_url = '/authorize'
-//     let client_id = '&client_id=d953cd347dda4a9d868ce4ff54ae8983'
-//     let response_type = '?response_type=code'
-//     let redirect_uri = '&redirect_uri=localhost:3000/users/code'
-//     let state = '&state=34fFs29kd09'
-//     let scope = '&scope=user-read-private user-read-email playlist-modify-public playlist-modify-private playlist-read-collaborative playlist-read-private'
-    
-//     let code_request_url = base_url + authentication_url + response_type + client_id + scope + redirect_uri + state
-    
-//     let response = await api.get(code_request_url).catch(err => { console.log(err) })
-//     console.log(response)
-    
-//     res.redirect(code_request_url)
-//     console.log(response);
-// });
-
-// app.get(['/code', '/:'], async function (req, res) {
-//     let code = req.query.code
-//     let token_base_url = 'https://accounts.spotify.com/api/token'
-//     let grant_type = 'authorization_code'
-//     let redirect_uri = 'localhost:3000/users/code'
-  
-  
-//     let access_token = await api.request({
-//       url: token_base_url,
-//       method: 'post',
-//       params: {
-//         'grant_type': grant_type,
-//         'code': code,
-//         'redirect_uri': redirect_uri,
-//       },
-  
-  
-//       headers:
-//       {
-//         'content-type': 'application/x-www-form-urlencoded',
-//         'authorization': 'Basic ' + "69b2b6da93404c5997ef18c4ef9ebbe4"
-//       }
-  
-  
-  
-//     }).catch(err => console.log(err))
-  
-  
-  
-//     let username = await api.request({
-//       url: 'https://api.spotify.com/v1/me',
-//       method: 'get',
-  
-  
-//       headers:
-//       {
-//         'content-type': 'application/x-www-form-urlencoded',
-//         'authorization': 'Bearer ' + access_token.data.access_token
-//       }
-//     }).catch(err => console.log(err))
-  
-  
-//     res.cookie('session_user', username.data.id)
-//     res.cookie('access_token', access_token.data.access_token)
-//     res.cookie('refresh_token', access_token.data.refresh_token)
-//     // res.render('index', { title: "Spotify Themes App" })
-//     res.render('about');
-  
-//     console.log(access_token);
-  
-//   })
+    const userInfo = await endPointData('/me');
+    const recInfo = await endPointData('/recommendations');
+    // console.log(userInfo);
+     //consider inserting into db, pull
+    res.render('search', { user: userInfo });
+});
 
                         
 app.listen(PORT, () => console.log(`listening on port ${PORT}`)); 
