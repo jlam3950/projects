39afc1933aebd2ac61baf3c7b45fcb7e076fc657
Jeffrey Lamar <jlam3950@gmail.com>
2022-07-09 12:26:27 -0400

    updated playlist endpoint to pull favorited songs from user

--- SpotifyAPP/server.js
+++ SpotifyAPP/server.js
@@ -1,27 +1,28 @@
 require('dotenv').config();
 const { response } = require('express');
 const express = require('express');
-// const spotifyRoutes = require('./src/spotify/routes')
+const { render } = require('express/lib/response');
 const app = express();
 const PORT = 3000; 
 const fetch = require('node-fetch');
-// const db = require('./src/spotify/queries');
 const pool = require('./db');
+// const spotifyRoutes = require('./src/spotify/routes')
+// const db = require('./src/spotify/queries');
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static('public'));
 app.use(express.json());
-// app.use('/api/spotify', spotifyRoutes);
 app.set('view engine', 'ejs');
+// app.use('/api/spotify', spotifyRoutes);
 
 const redirect_uri = 'http://localhost:3000/callback';
 const client = process.env.CLIENT_ID; 
 const secret = process.env.CLIENT_SECRET;
 let access_token;
 let userInfo; 
+let playlist = [];
 
 app.get('/', (req,res) => {
-    
     // pool.query('SELECT * FROM users WHERE user_id=1', (err,res)=>{
     //     if(!err){
     //         console.log(res.rows);
@@ -32,19 +33,15 @@ app.get('/', (req,res) => {
     res.render('index');
 })
 
-app.post('/'), (req,res) => {
-
-}
-
 app.get('/authorize', (req,res) => {
-    const auth_query_parameters = new URLSearchParams({
+    const auth_parameters = new URLSearchParams({
         response_type: 'code',
         client_id: client, 
         scope:'user-read-private user-read-email',
         redirect_uri: 'http://localhost:3000/callback',
         state: '34fFs29kd09'
     })
-    res.redirect("http://accounts.spotify.com/authorize?" + auth_query_parameters.toString());
+    res.redirect("http://accounts.spotify.com/authorize?" + auth_parameters.toString());
 });
 
 app.get('/callback', async (req,res) => {
@@ -68,11 +65,11 @@ app.get('/callback', async (req,res) => {
 
     const data = await response.json(); 
     access_token = data.access_token;
-    console.log('Bearer ' + access_token);
+    // console.log('Bearer ' + access_token);
     res.redirect('search');
 });
 
-async function endPointData(){
+async function addUser(){
     const response = await fetch('https://api.spotify.com/v1/me', {
         method: 'GET',
         headers: {
@@ -91,58 +88,53 @@ async function endPointData(){
 }
 
 app.get('/search', async (req,res) => {
-
-    const userInfo = await endPointData(); 
+    const userInfo = await addUser(); 
     res.render('search', { user: userInfo});  
 });
-
-// app.post('/search', async (req,res) =>{
-//     try{
-//         userInfo = await endPointData('/me');
-//         console.log(userInfo);
-//          const spotifyID = userInfo.id;
-//          await pool.query('INSERT INTO users (username) VALUES ($1)', [ spotifyID ])
-//          console.log(res.rows);
-//         } catch (err){
-//         console.log(err);
-//     }
-// });
 
 app.get('/accesstoken', (req,res) =>{
     res.send({access_token});
 })
 
+
+
 app.get('/playlist', (req,res) => {
-    res.render('playlist');
-})
+    
+    // const id = parseInt(req.params.user_id);
+    pool.query("SELECT tracks FROM favorites", (err, res) =>{
+        if(!err){
+            console.log(res.rows);
+            let playlistArray = res.rows;
+            playlist = playlistArray.map(a => a.tracks);
+            console.log(playlist);
+            // render(playlist);
+            // console.log(playlist.track)
+        }else{
+            console.log(err);
+        }
+    // const userPlaylist = pool.query("SELECT track FROM testing WHERE user_id = ($1)", [id])
+    // res.render('playlist', { playlist: userPlaylist });
+    })
+    // (error, results) => {
+    // pool.query("SELECT * FROM testing WHERE user_id = $1", [id], (error, results) => {
+    // if (error) throw error;
+    //     res.status(200).json(results.rows);
+    res.render('playlist', { playlist });
+    });
 
-app.post('/playlist', async (req,res) => {
+
+app.post('/search', async (req,res) => {
     try{
-    const {track} = req.body;
-    console.log(track);
-    const newTrack = await pool.query("INSERT INTO testing (track_name) VALUES ($1)," [ track ])
-    console.log(res.json(newTrack));
-    } catch(err){
-        console.log(err);
-    }
-})
+        const {track} = req.body;
+        if(track == null){
+            console.log("Track is null");
+            return;
+        }
+        const newTrack = await pool.query("INSERT INTO favorites (tracks, user_id) VALUES ($1,$2)", [ track, 1 ])
+        // console.log(newTrack);
+        } catch(err){
+            console.log(err);
+        }
+})  
 
-  
-
-
-// const addTrack = (req,res) => {
-//     const { track } = req.body; 
-//     // pool.query("SELECT s FROM users s WHERE s.email = $1", [email], (error, results) => {
-//         //     if(results.rows.length) {
-//             //         res.send('Email already exists.')
-//             //     }
-            
-//             pool.query("INSERT INTO users (track - artist) VALUES ($1)", [ track ], (error,results) =>{
-//                 if (error) throw error;
-//                 res.status(201).send('User Created Successfully!');
-//                 console.log('User created')
-//             })
-//             // });
-//         };
-        
 app.listen(PORT, () => console.log(`listening on port ${PORT}`)); 
