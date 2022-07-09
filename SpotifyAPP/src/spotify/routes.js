fc5b5b8a224a984349862f510f2e30b07d59471f
Jeffrey Lamar <jlam3950@gmail.com>
2022-07-09 12:27:54 -0400

    adding background images, and ejs file updates

--- SpotifyAPP/src/spotify/routes.js
+++ SpotifyAPP/src/spotify/routes.js
@@ -1,10 +1,10 @@
-const {Router} = require('express');
-const router = Router();
-const controller = require ('./controller');
+// const {Router} = require('express');
+// const router = Router();
+// const controller = require ('./controller');
 
-router.get('/', controller.getSpotify);
-router.post('/', controller.addUser);
-router.get('/:user_id', controller.getUserbyId);
-// router.delete('/:user_id', controller.removeUser);
+// router.get('/', controller.getSpotify);
+// router.post('/', controller.addUser);
+// router.get('/:user_id', controller.getUserbyId);
+// // router.delete('/:user_id', controller.removeUser);
 
-module.exports = router; 
+// module.exports = router; 
